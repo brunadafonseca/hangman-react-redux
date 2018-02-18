14311579d72e29365965c7eca6f133fb929dd67e
brunadafonseca <brunadafonseca@gmail.com>
2018-02-18 14:31:45 +0100

    Refactoring game container

--- src/game/GameContainer.js
+++ src/game/GameContainer.js
@@ -1,9 +1,10 @@
 import React, { PureComponent, Fragment } from 'react'
+import { connect } from 'react-redux'
+import { replace } from 'react-router-redux'
 import Photo from './Photo'
 import GuessForm from './GuessForm'
 import Word from './Word'
-import { connect } from 'react-redux'
-import { replace } from 'react-router-redux'
+
 
 class GameContainer extends PureComponent {
 
@@ -32,17 +33,18 @@ class GameContainer extends PureComponent {
     return(
       <div>
         <h1>Hangman</h1>
+
         <Photo photoId={ wrongGuessCount } playerWon={this.playerWon()}/>
         { this.playerLost() ? <Word word={ secretWord }/> :
                               <Word word={ guessedWord } /> }
-                                
-        { !this.endGame() && <Fragment>
-                             <GuessForm {...this.props.hangman}/>
-                             <h3>Guessed Letters:</h3>
-                             <p>{guessedLetters.join(", ")}</p>
-                           </Fragment>}
 
-        { (this.playerWon() || this.playerLost()) && <button onClick={this.restartGame}>Restart Game</button> }
+        { !this.endGame() && <Fragment>
+                               <GuessForm {...this.props.hangman}/>
+                               <h3>Guessed Letters:</h3>
+                               <p>{guessedLetters.join(", ")}</p>
+                             </Fragment>}
+
+        { (this.endGame()) && <button onClick={this.restartGame}>Restart Game</button> }
       </div>
     )
   }
